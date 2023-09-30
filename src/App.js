555100d39e4aba8f277bc60a1d5847234c32d9a6
Tony Duncombe <minetdd@gmail.com>
2023-09-29 19:56:38 -0600

    Update app js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import FormBuilder from './components/formBuilder';
 import FormContainers from './components/formContainer';
 
 class App extends Component {
@@ -8,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <FormBuilder /> */}
         <FormContainers />
       </div>
     );
